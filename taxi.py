b1c9879e669409451a9a48ba9fbdb7b4991a2468
James Xia <113453761+jamsxia@users.noreply.github.com>
2024-07-15 18:08:04 -0400

    Add files via upload

--- taxi.py
+++ taxi.py
@@ -3,6 +3,8 @@ from ql import QLAgent
 import matplotlib.pyplot as plt
 
 env = gym.make("Taxi-v3")
+env.render()
+env.reset() # reset environment to a new, random state
 agent=QLAgent(env)
 
 agent.play()
